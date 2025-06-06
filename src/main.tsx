568cb6b60ac4491d87264935218a536daf02bda8
Yuri Quill <yuri.i.tdevs@gmail.com>
2025-06-06 05:55:12 +0300

    feat: integrate Redux store and hooks for state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import { store } from "@/store/store";
 import router from "@/router";
 
 import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+         <RouterProvider router={router} />
+      </Provider>
    </StrictMode>
 );
